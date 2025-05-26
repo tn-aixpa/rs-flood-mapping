7b59ca53cc75c44d9a4daea6cc07df7c074763d6
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-26 16:38:09 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     datapath = 'data'
     outpath = 'output'
     sentinel_zip_path = 'data/sentinel_zips'
-    output_folder = 'data/flood_mask'
+    output_folder = 'data/flood_outputs'
 
     input1 = json_input['input1']
     project_name=os.environ["PROJECT_NAME"]
@@ -137,6 +137,8 @@ if __name__ == "__main__":
     sentinel_artifact = project.get_artifact(input2)
     sentinel_zip_path = sentinel_artifact.download(sentinel_zip_path, overwrite=True) 
     
+    print(f"Running flood detection pipeline with inputs: {sentinel_zip_path}, {shapfile_path}, {output_folder}")
+
     run_pipeline_zip(sentinel_zip_path, shapfile_path, output_folder)
 
     print(f"Upoading artifact: {artifact_name}, {artifact_name}")
