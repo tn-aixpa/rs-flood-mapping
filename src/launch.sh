82c3550ddf69f99dc7280496abfb6d0d8e7cde17
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-28 14:43:40 +0200

    Fixes for pipeline

--- src/launch.sh
+++ src/launch.sh
@@ -8,5 +8,5 @@ gdal-config --version
 python --version
 export PATH="/home/nonroot/miniforge3/snap/bin:$PATH"
 cd /app
-python main.py "{'input1':'$1', 'input2':'$2','input3':'$3','input4':'$4'}"
+python main.py "{'input1':'$1', 'input2':'$2','input3':'$3','input4':'$4','input5':'$5','input6':'$6','input7':'$7','input8':'$8','input9':'$9','input10':'${10}','input11':'${11}','input12':'${12}','input13':'${13}','input14':${14},'input15':${15},'input16':${16},'input17':${17}}"
 exit
