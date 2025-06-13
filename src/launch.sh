c4a06eaa23268b4b21602bfc677a74ba38aca005
Nawaz Khurshid <nawaz1981@gmail.com>
2025-06-13 23:11:07 +0200

    Update launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -17,7 +17,7 @@ echo $GDAL_DATA
 echo "PROJ_LIB AFTER EXPORT"
 echo $PROJ_LIB
 echo "Running flood mapping script with parameters:"
-echo "{'s1PreFlood': '$1', 's1PostFlood':'$2', 's2PreFlood':'$3','s2PostFlood':'$4','geomWKT':'$5','slopeArtifact':'$6','slopeFileName':'$7','lakeShapeArtifactName':'$8','lakeShapeFileName':'$9','riverShapeArtifactName':'${10}','riverShapeFileName':'${11}','output':'${12}','eventDate':'${13}','targetCRS':'${14}','polarization':${15},'dem_threshold':${16},'slope_threshold':${17},'noise_min_pixels':${18},'river_buffer_meters':${19}}"
+echo "{'s1PreFlood': '$1', 's1PostFlood':'$2', 's2PreFlood':'$3','s2PostFlood':'$4','geomWKT':'$5','slopeArtifact':'$6','slopeFileName':'$7','lakeShapeArtifactName':'$8','lakeShapeFileName':'$9','riverShapeArtifactName':'${10}','riverShapeFileName':'${11}','output':'${12}','eventDate':'${13}','targetCRS':'${14}','polarization':${15},'dem_threshold':${16},'slope_threshold':'${17}','noise_min_pixels':${18},'river_buffer_meters':${19}}"
 cd /app
 python main.py "{'s1PreFlood':'$1', 's1PostFlood':'$2', 's2PreFlood':'$3','s2PostFlood':'$4','geomWKT':'$5','slopeArtifact':'$6','slopeFileName':'$7','lakeShapeArtifactName':'$8','lakeShapeFileName':'$9','riverShapeArtifactName':'${10}','riverShapeFileName':'${11}','output':'${12}','eventDate':'${13}','targetCRS':'${14}','polarization':${15},'dem_threshold':${16},'slope_threshold':'${17}','noise_min_pixels':${18},'river_buffer_meters':${19}}"
 exit
